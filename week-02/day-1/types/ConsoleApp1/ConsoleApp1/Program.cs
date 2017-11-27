8c5979e6fe515aeed2c0bd470d0feef9d77d879f
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 15:44:05 +0100

    feat(Types cs project)types excersise upload

---
+++ week-02/day-1/types/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(42);
+
+            Console.Write(3.1415);
+
+            Console.Read();
+        }
+    }
+}
