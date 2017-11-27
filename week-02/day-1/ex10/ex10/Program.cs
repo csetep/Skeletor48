b50504f44558ae7a1357cea96982fb855ae964b5
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 17:30:21 +0100

    feat(C# excersise10)excersise upload

---
+++ week-02/day-1/ex10/ex10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Misi";
+            int MyAge = 33;
+            double MyHeight = 1.94;
+            bool married = true;
+
+            Console.WriteLine(MyName);
+            Console.WriteLine(MyAge);
+            Console.WriteLine(MyHeight);
+
+            if (married == true)
+            {
+                Console.WriteLine("married");
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
