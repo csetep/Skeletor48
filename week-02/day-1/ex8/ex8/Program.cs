3fca5cc2f75118d81e407000b5957b2af16eb520
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 17:04:00 +0100

    feat(C# excersise8)excersise upload

---
+++ week-02/day-1/ex8/ex8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("a is :" + a);
+            Console.WriteLine("b is :" + b);
+            Console.ReadLine();
+        }
+    }
+}
