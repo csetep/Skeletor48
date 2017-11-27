1ce828e4a31c083f14bd652db4768d4f21c4f9fc
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 16:10:55 +0100

    feat(C# excersise5)excersise upload

---
+++ week-02/day-1/ex5/ex5/ex5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 13;
+            int number2 = 22;
+
+            Console.WriteLine(number1 + number2);
+            Console.WriteLine(number2 - number1);
+            Console.WriteLine(number1 * number2);
+            Console.WriteLine(number2 / (double)number1);
+            Console.WriteLine(number2 / number1);
+            Console.WriteLine(number2 % number1);
+            Console.ReadLine();
+        }
+    }
+}
