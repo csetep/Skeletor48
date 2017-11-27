c2e315ab0e5f8a9698afeccec7d6a6773f74e44f
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 23:36:25 +0100

    feat(C# excersise12)excersise upload

---
+++ week-02/day-1/ex12/ex12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 32;
+            double b = 28;
+            double c = 10;
+
+            double surface = 2 * (a * b + a * c + b * c);
+            double volume = a * b * c;
+
+            Console.WriteLine("Surface Area: " + surface);
+            Console.WriteLine("Volume: " + volume);
+
+            Console.ReadKey();               
+                  
+        }
+    }
+}
