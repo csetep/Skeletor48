1499bf445413bcd88c39d026b054cae610b6af26
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 16:41:23 +0100

    feat(C# excersise6)excersise upload

---
+++ week-02/day-1/ex6/ex6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int week = 17;
+            int workdays = 5;
+            int hoursperday = 6;
+            int weeklyaveragehours = 52;
+            int allhours = week * weeklyaveragehours;
+            int allcoding = week * workdays * hoursperday;
+
+            Console.WriteLine(allcoding);
+            Console.WriteLine((allcoding/(double)allhours)*100);
+            Console.ReadLine();
+        }
+    }
+}
