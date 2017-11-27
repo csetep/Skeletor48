ac74590e5bf060dd7e121ed4a4be4351118f22af
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-27 16:55:36 +0100

    feat(C# excersise7)excersise upload

---
+++ week-02/day-1/ex7/ex7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favorite = 8;
+
+            Console.WriteLine("My favourite number is:" + favorite);
+            Console.ReadLine();
+        }
+    }
+}
