4a7379a12f87e662bdf56a2e9855de2986050272
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-28 00:02:51 +0100

    feat(C# excersise13)excersise upload

---
+++ week-02/day-1/ex13/ex13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            int twentyfourhour = 24 * 60 * 60;
+            int currenttime = 14 * 60 * 60 + 34 * 60 + 42;
+            int targetTime = twentyfourhour - currenttime;
+
+            Console.WriteLine(targetTime);
+            Console.WriteLine(twentyfourhour);
+            Console.WriteLine(currenttime);
+
+            for(int i =0; i <= targetTime; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadKey();
+       
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+        }
+    }
+}
