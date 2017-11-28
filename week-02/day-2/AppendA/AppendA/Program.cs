4e46591f00616f97464ce95d62634dc4b1353626
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-28 14:15:09 +0100

    feat(C#-day2)finish'AppendA'

---
+++ week-02/day-2/AppendA/AppendA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+
+            appendA(am);
+            Console.ReadLine();
+        }
+
+        static void appendA(string name)
+        {
+            Console.WriteLine(name + "a");
+        }
+
+    }
+}
