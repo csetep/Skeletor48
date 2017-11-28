ca77fa2e23acc5b8c4450c813d65d6bf2de087fd
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-28 12:12:54 +0100

    feat(C#-day2)finish'Doubling'

---
+++ week-02/day-2/Doubling/Doubling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+        }
+
+        public static int Doubling(int number)
+        {
+            return (number *= 2);
+        }
+    }
+}
