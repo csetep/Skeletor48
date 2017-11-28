2073857b3df33ec84bd446c16f0aea288cfab9ab
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-28 15:23:17 +0100

    feat(C#-day2)finish'Summing'

---
+++ week-02/day-2/Summing/Summing/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The sum of numbers up to {0}", num);
+            Console.WriteLine(Sum(num));
+
+            Console.ReadLine();
+
+        }
+        public static int Sum(int number)
+        {
+            return (number * (number + 1)) / 2;
+               }
+         
+    }
+}
