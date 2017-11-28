ae476446a323f70d6300a9162132a3a7c53a886d
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-28 12:37:47 +0100

    feat(C#-day2)finish'Greet'

---
+++ week-02/day-2/Greet/Greet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+
+            Greet(al);
+            Console.ReadLine();
+        }
+
+        static void Greet(string name)
+        {
+            Console.WriteLine("Greetings dear," + name);
+        }
+    }
+}
+
