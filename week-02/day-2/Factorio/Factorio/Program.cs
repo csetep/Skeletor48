8ee2f2c6556a93faa3880dfd548190958f5197f6
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-28 17:20:59 +0100

    feat(C#-day2)finish'Factorio'

---
+++ week-02/day-2/Factorio/Factorio/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The factorial of {0}", num);
+            Console.WriteLine(Factorio(num));
+
+            Console.ReadLine();
+
+        }
+        static double Factorio(int fact)
+        {
+            int x = fact;
+
+            for (int i = 1; i < fact; i++)
+            {
+                x = x * i;
+            }
+
+            return x;
+
+
+        }
+    }
+}
