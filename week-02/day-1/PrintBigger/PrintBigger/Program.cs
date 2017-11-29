9a2b311aa895ad191f07f948805ca5e72f40141b
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 00:00:11 +0100

    feat(C#-day1)'finish PrintBigger'

---
+++ week-02/day-1/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Give me two number and press enter! ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine(" {0} is bigger ! ", number1);
+
+            }
+            else if (number1 < number2)
+            {
+                Console.WriteLine(" {0} is bigger ! ", number2);
+            }
+            else
+            {
+                Console.WriteLine(" {0} and {1} are equals! ", number1, number2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
