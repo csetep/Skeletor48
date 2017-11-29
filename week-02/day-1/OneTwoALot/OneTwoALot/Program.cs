362a0cf21b96e0fce0edc2374ce281adc7b3067f
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 23:38:34 +0100

    feat(C#-day1)'finish OneTwoALot'

---
+++ week-02/day-1/OneTwoALot/OneTwoALot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" Give me a number and press enter! ");
+            double number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 0)
+            {
+                Console.WriteLine("Not enough!");
+            }
+            else if (number == 1)
+            {
+                Console.WriteLine("One!");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Two!");
+            }
+            else
+            {
+                Console.WriteLine("A lot!");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
