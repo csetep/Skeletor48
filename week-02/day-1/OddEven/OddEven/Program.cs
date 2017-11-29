4154bcfb934584b6a36f4b5bf76ff0bab3188686
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 23:25:48 +0100

    feat(C#-day1)'finish OddEven'

---
+++ week-02/day-1/OddEven/OddEven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" Give me a number and press enter! ");
+            double number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("This number is even!");
+            }
+            else
+            {
+                Console.WriteLine("This number is Odd!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
