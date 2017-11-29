813e471785738851748485dc6039686818095a92
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 22:19:16 +0100

    feat(C#-day1)'finish MileToKm'

---
+++ week-02/day-1/MileToKm/MileToKm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \t What is your name my human friend?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\n \t Give me the number of kilometers and I will convert it to miles for You {0}!", name);
+            int km = Convert.ToInt32(Console.ReadLine());
+
+            double mile = km / 1.609344;
+
+            Console.WriteLine("\n \t {0} kilometers is {1} miles dear {2}!", km, mile, name);
+            Console.ReadKey();
+        }
+    }
+}
