d9e59fd2d08cbab0e865d8b01b22d3029f37c6a8
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 16:51:53 +0100

    feat(C#-day3)finish'Reverse'

---
+++ week-02/day-3/Reverse/Reverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reverse
+{
+    public class Reverse
+    {
+        public static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            Console.WriteLine(ReReverse(reversed));
+            Console.ReadLine();
+        }
+
+        public static string ReReverse(string text)
+        {
+            string rereversed = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                rereversed += text[i];
+            }
+
+            return rereversed;
+        }
+    }
+}
