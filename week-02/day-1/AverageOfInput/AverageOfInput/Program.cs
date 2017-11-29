53a1ba8baec78a74b760fbec6a7e77e604e77763
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 23:11:38 +0100

    feat(C#-day1)'finish AverageOfInput'

---
+++ week-02/day-1/AverageOfInput/AverageOfInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageOfInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\tGive me five numbers in a row!\n\tPlease press enter after each number! ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2 + num3 + num4 + num5;
+            double average = sum / 5;
+
+            Console.WriteLine("\n\n\n\t The sum of the five numbers is {0} and the average of them is {1}. ", sum, average);
+            Console.ReadKey();
+        }
+    }
+}
