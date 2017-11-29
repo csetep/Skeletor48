e73239c5112ea304f2703bd9b787879bfb903327
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 15:42:41 +0100

    feat(C#-day3)finish'TodoPrint'

---
+++ week-02/day-3/TodoPrint/TodoPrint/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk";
+
+            var todoList = new List<string>();
+            todoList.Add("My todo:");
+            todoList.Add(todoText);
+            todoList.Add(" - Download games");
+            todoList.Add("\t - Diablo");
+
+            foreach (string listElement in todoList)
+            {
+                Console.WriteLine(listElement);
+            }
+
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            Console.ReadKey();
+        }
+    }
+}
