eb7193e325e15fe51d897714e9611aefc0f72508
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 00:26:12 +0100

    feat(C#-day1)'finish PartyIndicators'

---
+++ week-02/day-1/PartyIndicators/PartyIndicators/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" How many girls came? ");
+            int girls = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" How many boys came? ");
+            int boys = Convert.ToInt32(Console.ReadLine());
+
+            int attendes = girls + boys;
+            int idealMinimum = 20;
+
+            if (girls == boys && attendes >= idealMinimum)
+            {
+                Console.WriteLine("The party is exellent!");
+            }
+            else if (girls != boys && attendes >= idealMinimum && girls != 0)
+            {
+                Console.WriteLine("Quite cool party!");
+            }
+            else if (attendes < idealMinimum && girls != 0)
+            {
+                Console.WriteLine("Average party...");
+            }
+            else if (girls <= 0 && boys != 0)
+            {
+                Console.WriteLine("Sausage party :( ");
+            }
+            else
+            {
+                Console.WriteLine("meh.....");
+            }
+            Console.ReadKey();
+        }
+    }
+}
