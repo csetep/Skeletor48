f1260aa5888eda3504ec8e72b505b7ac1e39387b
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-29 14:42:15 +0100

    feat(C#-day3)finish'TakesLonger'

---
+++ week-02/day-3/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakesLonger
+{
+    public class TakesLonger
+    {
+        public static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+            string fix = "always takes longer than ";
+            int positionOfFix = quote.IndexOf("you expect");
+
+            var theHof = new StringBuilder();
+            theHof.Append(quote);
+            theHof.Insert(positionOfFix, fix);
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+            Console.WriteLine(theHof);
+            Console.ReadKey();
+        }
+    }
+}
+
+
