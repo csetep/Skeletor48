fa04b8c0c0eee10d6a09b235deee00943d880cd7
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 23:45:14 +0100

    feat(C#-day2)'delete unnecessary string PrintArguments'

---
+++ week-02/day-2/PrintArguments/PrintArguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] toPrint = { "Print", "this", "text", "for", "me", "!" };
+            Printer(toPrint);
+            Console.ReadLine();
+        }
+        static void Printer(string[] textToPrint)
+        {
+
+            foreach (string words in textToPrint)
+                Console.WriteLine(words);
+
+        }
+    }
+}
