a7b29e6581c40ed3ad115c1c14bf5c0fdc8f05b8
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-01 00:14:37 +0100

    feat(C#-day2):finish CompareLength

---
+++ week-02/day-2/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] p1 = { "1", "2", "3"};
+            string[] p2 = { "4", "5",};
+
+            bool more = true;
+
+            more = (p1.Length < p2.Length);
+
+            if (more)
+            {
+                Console.WriteLine("p2 has more elements than p1");
+            }
+            else if (more != true)
+            {
+                Console.WriteLine("p2 has less elements less than p1");
+            }
+            else
+            {
+                Console.WriteLine("p2 has equal numbers of elements to p1");
+            }
+            Console.ReadLine();
+        }
+    }
+}
