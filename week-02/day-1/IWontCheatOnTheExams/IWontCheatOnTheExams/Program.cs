2d40dde004b9403b8673ad0b94a47f40413a3cee
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 10:24:38 +0100

    feat(C#-day1)'finish IWontCheatOnExams'

---
+++ week-02/day-1/IWontCheatOnTheExams/IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine("\t" + i + "\tI won't cheat on the exam!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
