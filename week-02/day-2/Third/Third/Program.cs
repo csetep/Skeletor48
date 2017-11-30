a677b6b41f43db63da5705542ab0fc6420bd39f3
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 23:50:13 +0100

    feat(C#-day2):finish Third

---
+++ week-02/day-2/Third/Third/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] q = { "4", "5", "6", "7" };
+
+            Console.WriteLine(q[2]);
+            Console.ReadLine();
+        }
+    }
+}
