25386b338b0c6425821c898696e04c6bff804678
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 11:08:05 +0100

    feat(C#-day1)'finish CountFromTo'

---
+++ week-02/day-1/CountFromTo/CountFromTo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tGive me two numbers and then press Enter!");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number2 <= number1)
+            {
+                Console.WriteLine("\tThe second number should be bigger!");
+            }
+            else
+            {
+                for (int i = number1; i < number2; i++)
+                {
+                    Console.WriteLine("\n\t{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
