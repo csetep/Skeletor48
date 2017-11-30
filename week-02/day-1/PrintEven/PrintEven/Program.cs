b09d190e22112200f590491f38a30ad67ad5abfb
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 10:32:44 +0100

    feat(C#-day1)'finish PrintEven'

---
+++ week-02/day-1/PrintEven/PrintEven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 500; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
