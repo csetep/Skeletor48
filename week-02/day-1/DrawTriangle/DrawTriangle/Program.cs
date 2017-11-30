04fc2aa3577eab9c675f9ba0b4157504247b9945
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 10:03:26 +0100

    feat(C#-day1)'finish DrawTriangle'

---
+++ week-02/day-1/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int triangle = Convert.ToInt32(Console.ReadLine());
+            string star = "*";
+
+            for (int i = 0; i < triangle; i++)
+            {
+                Console.WriteLine(star);
+                star += "*";
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
