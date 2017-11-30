2f315417ff509fe1391fd6c1c603befacd2dd580
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 13:12:36 +0100

    feat(C#-day1)'finish DrawPyramid'

---
+++ week-02/day-1/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = Convert.ToInt32(Console.ReadLine());
+
+            string space = " ";
+            string star = "*";
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int ii = i; ii < lines - 1; ii++)
+                {
+                    Console.Write(space);
+                }
+
+                Console.WriteLine(star);
+                star += "**";
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
