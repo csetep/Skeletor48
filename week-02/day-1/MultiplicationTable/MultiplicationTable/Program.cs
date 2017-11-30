fc43dc028b4886d51a65185ed1a54982c2ae7814
Skeletor48 <banfi.mihaly@gmail.com>
2017-11-30 10:54:55 +0100

    feat(C#-day1)'finish MultiplicationTable'

---
+++ week-02/day-1/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\tEnter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 10)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    int multiplication = i * number;
+                    Console.WriteLine("\t{0} * {1} = {2}", i, number, multiplication);
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    int multiplication = i * number;
+                    Console.WriteLine("\t{0} * {1} = {2}", i, number, multiplication);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
