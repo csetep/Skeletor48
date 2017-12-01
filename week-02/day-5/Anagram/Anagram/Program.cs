a7f3c67e868ae86e2c849b2b96a7e1134dcb1fdc
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-01 12:35:49 +0100

    feat(C#-day5):making anagram checker with no valid solution yet

---
+++ week-02/day-5/Anagram/Anagram/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me two words and than press enter!");
+            string inputone = (Console.ReadLine());
+            char[] charListOne = inputone.ToUpper().ToCharArray();
+
+            string inputtwo = (Console.ReadLine());
+            char[] charListTwo = inputtwo.ToUpper().ToCharArray();        
+
+            Array.Sort(charListOne);
+            Array.Sort(charListTwo);
+
+            //bool anagram = true;
+            //anagram = (charListOne.Length != charListTwo.Length);
+
+            if (charListOne.Length != charListTwo.Length)
+            {
+                Console.WriteLine("False!");
+            }
+            else
+            {
+                int count = 0;
+
+                foreach (char letters in charListTwo)
+                {
+                    if (letters == charListOne[count])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("False!");
+                        break;
+                    }
+                }
+
+            }
+
+
+            // AnagramChecker(charListOne, charListTwo);
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+       // static void AnagramChecker(char[] wordone, char[] wordtwo)
+        //{
+     //       Array.Sort(wordone);
+       //     Array.Sort(wordtwo);
+
+            
+        //}
+
+
+    }
+}
