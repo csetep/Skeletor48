9d6cbfe7b79a22274ad71af798c1fbf9bd0420af
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-01 01:18:30 +0100

    feat(C#-day2):finish PrintAll

---
+++ week-02/day-2/PrintAll/PrintAll/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+
+            // #1
+
+            af.ToList().ForEach(Console.WriteLine);
+
+            // #2
+
+            Console.WriteLine("[{0}]", string.Join(", ", af));
+
+            // #3
+
+            foreach (int numbers in af)
+            {
+                Console.WriteLine(numbers);
+            }
+            Console.ReadLine();
+        }
+    }
+}
