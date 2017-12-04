7e3b62d509dfccc0d26b0a4d78a50eb700cdd85e
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-04 12:24:55 +0100

    feat(C#-week3day1):finish PrintEachLine

---
+++ week-03/day-1/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                File.ReadAllText(@"my-file.txt");
+                Console.WriteLine(File.ReadAllText(@"my-file.txt"));
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
