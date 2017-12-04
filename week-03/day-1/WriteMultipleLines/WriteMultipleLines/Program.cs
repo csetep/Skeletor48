1fd3b47fe0f4b5dfe531fcc08190b4d3b7a99338
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-04 16:25:26 +0100

    feat(C#-week3day1):finish WriteMultipleLines

---
+++ week-03/day-1/WriteMultipleLines/WriteMultipleLines/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = "my-text.txt";
+
+                Console.WriteLine("Type in a word than press enter!");
+                string word = Console.ReadLine();
+
+                Console.WriteLine("Type in a number than press enter two times!");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                string[] content = { word };
+
+                Writer(path, content, number);
+
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+        static void Writer(string path, string[] content, int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                File.AppendAllLines(path, content);
+            }
+        }
+    }
+}
