e6a15e5dac6e4f4ef6233b5e2c442e75671c245f
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-04 14:24:31 +0100

    feat(C#-week3day1):finish CountLines

---
+++ week-03/day-1/CountLines/CountLines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "countlines.txt";
+
+            try
+            {
+                int numberOfLines = LineCounter(path);
+                Console.WriteLine(numberOfLines);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("0");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+
+        public static int LineCounter(string textFile)
+        {
+            StreamReader myText = new StreamReader(textFile);
+
+            int lineCount = 0;
+
+            while (myText.ReadLine() != null)
+            {
+                lineCount++;
+            }
+            return lineCount;
+        }
+    }
+}
