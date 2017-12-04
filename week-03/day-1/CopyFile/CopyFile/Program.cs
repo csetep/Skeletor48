e572eec6503cd6a848053e9c54a10926aaa369f2
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-04 17:35:44 +0100

    feat(C#-week3day1):working on CopyFile

---
+++ week-03/day-1/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader inputPath = new StreamReader("my-text1.txt");
+            StreamWriter outputPath = new StreamWriter("my-text2.txt");
+
+            CopyCat(inputPath, outputPath);
+
+            Console.ReadKey();
+        }
+
+        static void CopyCat(StreamReader input, StreamWriter output)
+        {
+            string line = "";
+
+            while ((line = input.ReadLine()) != null)
+            {
+                output.WriteLine(line);
+            }
+            output.Close();
+        }
+    }
+}
+
+
+
+
+// Write a function that reads all lines of a file and writes 
+// the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
