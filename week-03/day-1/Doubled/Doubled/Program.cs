178ac946abedfe31e3dda0f7d28e6f63469b5b47
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-05 00:56:46 +0100

    feat(C#-week3day1):almost finish Doubled

---
+++ week-03/day-1/Doubled/Doubled/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader encryptedText = new StreamReader("duplicated-chars.txt");
+            StreamWriter decryptedText = new StreamWriter("single-chars.txt");
+
+            Decryptor(encryptedText, decryptedText);
+
+            Console.ReadKey();
+        }
+
+        static void Decryptor(StreamReader source, StreamWriter decryptedText)
+        {
+            string empty = "";
+            string text = source.ReadToEnd();
+            char[] characters = text.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    empty += characters[i + 1].ToString();
+                }
+            }
+            decryptedText.WriteLine(empty);
+            decryptedText.Close();
+        }
+    }
+}
