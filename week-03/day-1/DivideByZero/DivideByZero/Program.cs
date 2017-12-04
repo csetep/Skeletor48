e32f7c7da6331c6576f84f932435f3de96913baf
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-04 11:51:22 +0100

    feat(C#-week3day1):finish DivideByZero

---
+++ week-03/day-1/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number");
+
+            try
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                int result = 10 / divisor;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("False");
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Something went wrong...");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
