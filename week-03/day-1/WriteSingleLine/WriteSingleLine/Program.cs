a04a1265e8b1cab9a0c522e228e41cadbaaf4380
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-04 14:54:08 +0100

    feat(C#-week3day1):finish WriteSingleLine

---
+++ week-03/day-1/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFile = @"my-file.txt";
+
+            try
+            {
+                WriteFile(myFile);
+                ReadFile(myFile);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+        static void WriteFile(string myFile)
+        {
+            File.WriteAllText(myFile, "Banfi Mihaly");
+        }
+
+        static void ReadFile(string myFile)
+        {
+            Console.WriteLine(File.ReadAllText(myFile));
+        }
+    }
+}
