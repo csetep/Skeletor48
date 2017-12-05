9b166ca6df98449fdf4ced2b466181acbbb1b8ab
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-05 23:05:32 +0100

    feat(C#-week2day1):finish DrawSquare

---
+++ week-02/day-1/DrawSquare/DrawSquare/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number!");
+            int number = int.Parse(Console.ReadLine());
+
+            string o = "o";
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (i == 0 || i == number - 1)
+                    {
+                        Console.Write(o);
+                    }
+                    else if (j == 0 || j == number - 1)
+                    {
+                        Console.Write(o);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
