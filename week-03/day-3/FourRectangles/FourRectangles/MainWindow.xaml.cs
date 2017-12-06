549b8079812410c44603472a4bd010446ad27b6b
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-06 14:35:13 +0100

    feat(C#-week3day3):finish FourRectangles

---
+++ week-03/day-3/FourRectangles/FourRectangles/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace FourRectangles
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+            DrawRectangles(foxDraw);
+        }
+
+        public static void DrawRectangles(FoxDraw foxDraw)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+            foxDraw.DrawRectangle(
+                (double)random.Next(200), 
+                (double)random.Next(200), 
+                (double)random.Next(200), 
+                (double)random.Next(200));
+            foxDraw.FillColor(Color.FromRgb(
+                (byte)random.Next(255),
+                (byte)random.Next(255),
+                (byte)random.Next(255)));
+            }
+        }       
+    }
+}
