8858f3b24cf793aea51fc4c311495e24526d407b
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-06 11:13:38 +0100

    feat(C#-week3day3):finish MiddleLines

---
+++ week-03/day-3/MiddleLines/MiddleLines/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace MiddleLines
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawRedHorizontalLine(foxDraw);
+            DrawGreenVerticalLine(foxDraw);
+
+
+        }
+
+        public static void DrawRedHorizontalLine(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 200, 400, 200);            
+        }
+
+        public static void DrawGreenVerticalLine(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(200, 0, 200, 400);
+        }
+    }
+}
