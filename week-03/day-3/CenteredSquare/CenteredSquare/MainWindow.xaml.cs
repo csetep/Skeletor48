d09449240ba08c576c477507ab5ca353801aad04
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-06 13:54:09 +0100

    feat(C#-week3day3):finish CenteredSquare

---
+++ week-03/day-3/CenteredSquare/CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace CenteredSquare
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+            DrawCenteredSquare(foxDraw);
+        }
+
+        public static void DrawCenteredSquare(FoxDraw foxDraw)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(195, 195));
+            points.Add(new Point(195, 205));
+            points.Add(new Point(205, 205));
+            points.Add(new Point(205, 195));
+
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.DrawPolygon(points);
+        }        
+    }
+}
