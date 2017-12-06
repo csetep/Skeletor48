66fc8be5e499e8aceb5b08cd6e72e8651cc812b2
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-06 15:21:34 +0100

    feat(C#-week3day3):finish PositionSquare

---
+++ week-03/day-3/PositionSquare/PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace PositionSquare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+            double x1 = 150.0;
+            double y1 = 150.0;
+
+            DrawThreeSquares(foxDraw, x1, y1);
+        }
+
+        public static void DrawThreeSquares(FoxDraw foxDraw, double x1, double y1)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                foxDraw.FillColor(Color.FromArgb(
+                    (byte)70,
+                    (byte)random.Next(255),
+                    (byte)random.Next(255),
+                    (byte)random.Next(255)));
+                foxDraw.DrawRectangle(x1, y1, 50, 50);
+
+                x1 += (double)random.Next(100);
+                y1 += (double)random.Next(100);
+            }
+        }
+    }
+}
