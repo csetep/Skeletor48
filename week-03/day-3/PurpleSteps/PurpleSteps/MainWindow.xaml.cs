de61b81ece92e50d0572605f1f9cfca6196fda56
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-06 23:21:50 +0100

    (C#-week3day3):finish PurpleSteps

---
+++ week-03/day-3/PurpleSteps/PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            int multiplier = 1;
+            int squarePosition = multiplier * 10;
+            int squareSize = squarePosition;
+
+            for (int i = 0; i < (40 / multiplier) - 1; i++)
+            {
+                DrawPurpleSquare(foxDraw, squarePosition + (i * squarePosition), squareSize);
+            }
+        }
+
+        public static void DrawPurpleSquare(FoxDraw foxDraw, int squarePosition, int squareSize)
+        {
+            int x1 = squarePosition;
+            int y1 = squarePosition;
+            int width = squareSize;
+            int height = squareSize;
+
+            foxDraw.FillColor(Color.FromRgb(250, 7, 226));
+            foxDraw.DrawRectangle(x1, y1, width, height);
+        }
+    }
+}
