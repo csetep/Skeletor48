efd9c8ab40f65a8fc43a46768fe028f88b9b49ae
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-06 17:40:40 +0100

    feat(C#-week3day3):finish RainbowBoxFunction

---
+++ week-03/day-3/RainbowBoxFunction/RainbowBoxFunction/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace RainbowBoxFunction
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+
+            double size = 400.0;
+            double numberOfColours = 7;
+            double downsize = (size / numberOfColours);
+
+            byte[] rArray = { 148, 75, 0, 0, 255, 255, 255 };
+            byte[] gArray = { 0, 0, 0, 255, 255, 127, 0 };
+            byte[] bArray = { 211, 130, 255, 0, 0, 0, 0 };
+
+            for (int i = 0; i < numberOfColours; i++)
+            {
+                byte r = rArray[i];
+                byte g = gArray[i];
+                byte b = bArray[i];
+
+                Color color = Color.FromRgb(r, g, b);
+
+                DrawBoxesCenter(foxDraw, size, color);
+
+                size -= downsize;
+            }
+        }
+
+        public static void DrawBoxesCenter(FoxDraw foxDraw, double size, Color color)
+        {
+            foxDraw.FillColor(color);
+            foxDraw.DrawRectangle(((400 - size) / 2), ((400 - size) / 2), size, size);
+        }
+    }
+}
