0bef6a323108a84ddd8393e39c46eadfec7c5545
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-07 11:54:49 +0100

    feat(C#-week3day4): Numberadder

---
+++ week-03/day-4/Numberadder/Numberadder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numberadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(NumberAdder(n));
+            Console.ReadKey();
+        }
+        public static int NumberAdder(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n + NumberAdder(n - 1);
+            }
+        }
+    }
+}
