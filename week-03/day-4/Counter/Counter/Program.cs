126c73a24bbce21d0bc91b9496e8842dd189f182
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-07 11:19:01 +0100

    feat(C#-week3day4): finish Counter

---
+++ week-03/day-4/Counter/Counter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            CountDown(n);
+            Console.ReadKey();
+        }
+        public static void CountDown(int n)
+        {
+            int counter = n;
+
+            if (counter >= 0)
+            {
+                Console.WriteLine(n);
+                CountDown(counter - 1);
+            }
+        }
+    }
+}
