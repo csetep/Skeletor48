a8b31e5fa2b73eddaafea39137e6bf8ee7547b8d
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-07 13:20:13 +0100

    feat(C#-week3day4): finish Sumdigit

---
+++ week-03/day-4/Sumdigit/Sumdigit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sumdigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Sumdigit(n));
+            Console.ReadKey();
+        }
+        public static int Sumdigit(int n)
+        {
+            if (n==0)
+            {
+                return 0;
+            }
+            else if (n<10)
+            {
+                return n%10;
+            }
+            else
+            {
+                return Sumdigit(n / 10) + n % 10;
+            }
+        }
+    }
+}
