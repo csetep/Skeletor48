093c76d754988d62adee2c8b5c5c8b76da1f7dc8
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-07 14:10:07 +0100

    feat(C#-week3day4): finish Bunny1

---
+++ week-03/day-4/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBunnies = int.Parse(Console.ReadLine());
+            int numberOfEars = 2;
+
+            Console.WriteLine(BunnyEarCounter(numberOfBunnies, numberOfEars));
+            Console.ReadKey();
+        }
+        public static int BunnyEarCounter(int numberOfBunnies, int numberOfEars)
+        {
+            if (numberOfBunnies == 1)
+            {
+                return numberOfEars;
+            }
+            else
+            {
+                return BunnyEarCounter(numberOfBunnies - 1, numberOfEars + 2);
+            }
+        }
+    }
+}
