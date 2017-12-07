b9e9f7574dccde03b2cb73ed8711b75dfc1e6256
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-07 17:21:27 +0100

    feat(C#-week3day4): left square works in Graphic

---
+++ week-03/day-4/Graphic/Graphic/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Graphic
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+
+            double squareX = 0;
+            double squareY = 0;
+            double squareSize = 603;
+            double counter = 0;
+            DrawSquare(foxDraw, squareX, squareY, squareSize, counter);
+        }
+
+        public static void DrawSquare(FoxDraw foxDraw, double squareX, double squareY, double squareSize, double counter)
+        {
+            if (counter < 9)
+            {
+                foxDraw.FillColor(Color.FromArgb(60, 254, 124, 34));
+                foxDraw.DrawRectangle(squareX, squareY, squareSize, squareSize);
+                counter++;
+
+                DrawSquare(foxDraw, squareX, (squareY + squareSize / 3), squareSize / 3, counter);
+            }
+        }
+    }
+}
