3b07efae8f340f9beeaf4237f8a0812feb479d16
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-12 12:30:22 +0100

    feat(C#-week4day2): finish Person Class in GreenFox Org

---
+++ week-04/day-2/GreenFoxOrg/GreenFoxOrg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreenFoxOrg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
