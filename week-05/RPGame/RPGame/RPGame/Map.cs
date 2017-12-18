5f9f19a572a25ebd80dad106f155ffc2560cf4d8
Skeletor48 <banfi.mihaly@gmail.com>
2017-12-18 15:44:40 +0100

    feat(C#-Week-5-RPGame)construct map with floor tiles and moving hero

---
+++ week-05/RPGame/RPGame/RPGame/Map.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using GreenFox;
+
+namespace RPGame
+{
+    class Map
+    {
+        public void MapBuilder(FoxDraw foxDraw)
+        {
+            double coordinateX = 0;
+            double coordinateY = 0;
+
+            for (int x = 0; x < 10; x++)
+            {
+                coordinateX =x* 50;
+
+                for (int y = 0; y < 10; y++)
+                {
+                    coordinateY = y* 50;
+                    foxDraw.AddImage("Assets/floor.png", coordinateX, coordinateY);
+                }
+            }
+        }
+    }
+}
