c3e64d6ab7dad9bfff5fb773be80cc37e56e065e
Skeletor48 <banfi.mihaly@gmail.com>
2018-01-02 16:12:23 +0100

    feat(C#-Week6-Day1-Comparable) compares domino tiles and sort them based on their values

---
+++ week-06/day-1/DominoeComparable/Dominoes/Domino.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Domino
+{
+    public class Domino : IComparable<Domino>
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int CompareTo(Domino nextDomino)
+        {
+            if (Values[0] != nextDomino.Values[0])
+            {
+                if (Values[0] > nextDomino.Values[0])
+                {
+                    return 1;
+                }
+                else if (Values[0] < nextDomino.Values[0])
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if ((Values[0] == nextDomino.Values[0]))
+            {
+                if (Values[1] > nextDomino.Values[1])
+                {
+                    return 1;
+                }
+                else if (Values[1] < nextDomino.Values[1])
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
---
+++ week-06/day-1/DominoeComparable/Dominoes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domino
+{
+    public class Dominoes
+    {
+        public static void Main(string[] args)
+        {
+            var dominoes = InitializeDominoes();
+
+            dominoes.Sort();
+
+            foreach (var domino in dominoes)
+            {
+                Console.WriteLine(domino.GetValues()[0] + " " + domino.GetValues()[1] + " | ");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>
+            {
+                new Domino(5, 2),
+                new Domino(4, 6),
+                new Domino(1, 5),
+                new Domino(6, 7),
+                new Domino(2, 4),
+                new Domino(7, 1),
+                new Domino(6, 4)
+            };
+            return dominoes;
+        }
+    }
+}
