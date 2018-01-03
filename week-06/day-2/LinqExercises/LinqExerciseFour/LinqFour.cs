b7c4afa98dbd7b30b57a28d51fe31bd707c9ffca
Skeletor48 <banfi.mihaly@gmail.com>
2018-01-03 15:49:24 +0100

    feat(C#-Week6-Day2-Linq-4) filtering numbers based on lambda expressions

---
+++ week-06/day-2/LinqExercises/LinqExerciseFour/LinqFour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExerciseFour
+{
+    class LinqFour
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var filteredNumbers = number.Where(n => (n * n) > 20).ToList();
+            filteredNumbers.ForEach(i => Console.WriteLine(i));
+
+            Console.ReadKey();
+        }
+    }
+}
