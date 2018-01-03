9c4c02bce9a8ecfabafa1521a94cce4cbfc7a14a
Skeletor48 <banfi.mihaly@gmail.com>
2018-01-03 17:38:55 +0100

    feat(C#-Week6-Day2-Linq-6) counts letters in string with both syntax

---
+++ week-06/day-2/LinqExercises/LinqExerciseSix/LinqSix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExerciseFive.cs
+{
+    class LinqFive
+    {
+        static void Main(string[] args)
+        {
+            string characters = "Be yourself; everyone else is already taken";
+
+
+            var occurrenceOfCharacters = from cha in characters
+                                         orderby cha ascending
+                                         group cha by cha into c
+                                         select new { c.Key, Count = c.Count() };
+
+            var occurenceOfCharactersTwo = characters.OrderBy(c => c).GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count());
+
+            occurrenceOfCharacters.ToList().ForEach(i => Console.WriteLine(i));
+            occurenceOfCharactersTwo.ToList().ForEach(i => Console.WriteLine(i));
+
+            Console.ReadKey();
+        }
+    }
+}
