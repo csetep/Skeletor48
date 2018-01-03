d4bd5ff901dbe226b7d91e676f5d8d9fcb138301
Skeletor48 <banfi.mihaly@gmail.com>
2018-01-03 15:40:50 +0100

    feat(C#-Week6-Day2-Linq-3) squares positive numbers

---
+++ week-06/day-2/LinqExercises/LinqExerciseThree/LinqThree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExerciseThree
+{
+    class LinqThree
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var squaredPositiveNumbers = numbers.Where(n => n >= 0)
+                .Select(n => n * n)
+                .ToList();
+
+            squaredPositiveNumbers.ForEach(i => Console.WriteLine(i));
+
+            Console.ReadKey();
+        }
+    }
+}
