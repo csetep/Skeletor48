fbfc89486652f424b48f3efa4b3c0fe1454249bf
Skeletor48 <banfi.mihaly@gmail.com>
2018-01-03 17:07:33 +0100

    feat(C#-Week6-Day2-Linq-5) counts frequency with method syntax

---
+++ week-06/day-2/LinqExercises/LinqExerciseFive.cs/LinqFive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExerciseFive.cs
+{
+    class LinqFive
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var occurrenceOfNumbers = from num in numbers
+                                      orderby num ascending
+                                      group num by num into n
+                                      select new { n.Key, Count = n.Count() };
+
+            occurrenceOfNumbers.ToList().ForEach(i => Console.WriteLine(i));
+
+            Console.ReadKey();
+        }
+    }
+}
